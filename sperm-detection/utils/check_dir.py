f990cdbd337b767443319726f418f5d465fb93f9
mustafa-onur-kilinc <mustafaonurkilinc00@gmail.com>
2024-05-21 12:25:06 +0300

    Added sperm-detection\utils directory
    This directory includes various scripts that are used in making datasets from videos, getting label information from JSON files, changing format of labels, performing dataset splitting, iterating through a directory with multiple 'run' directories and pausing operations to give computer GPU a chance to cooldown

---
+++ sperm-detection/utils/check_dir.py
@@ -0,0 +1,34 @@
+"""
+Script by Özgün Zeki BOZKURT
+"""
+
+import os
+from pathlib import Path
+
+
+def get_next_run_directory(base_path):
+    # Ensure the base path exists
+    Path(base_path).mkdir(parents=True, exist_ok=True)
+
+    # List all existing runs
+    existing_runs = [
+        d
+        for d in os.listdir(base_path)
+        if os.path.isdir(os.path.join(base_path, d)) and d.startswith("run_")
+    ]
+
+    # Find the highest existing run number
+    highest_run = 0
+    for run in existing_runs:
+        try:
+            run_number = int(run.split("_")[1])
+            highest_run = max(highest_run, run_number)
+        except ValueError:
+            # Skip directories that do not follow the expected naming convention
+            continue
+
+    # Define the next run directory
+    next_run_number = highest_run + 1
+    next_run_dir = os.path.join(base_path, f"run_{next_run_number}")
+
+    return next_run_dir
